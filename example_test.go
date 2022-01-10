ae17578056bb70a5b98885f4eec7d5f47941d2f1
Alexander Yastrebov <yastrebov.alex@gmail.com>
2022-01-10 17:32:11 +0100

    Adds module

---
+++ example_test.go
@@ -0,0 +1,15 @@
+package stream_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStream(t *testing.T) {
+	t.Log("test")
+}
+
+func ExampleStream() {
+	fmt.Printf("hello")
+	// Output: hello
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AlexanderYastrebov/stream
+
+go 1.18
