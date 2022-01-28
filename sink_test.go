20b34e473ff57fa4e772a70626e4dc64c891d6d3
Alexander Yastrebov <yastrebov.alex@gmail.com>
2022-01-28 03:32:00 +0100

    Adds testSink

--- sink_test.go
+++ sink_test.go
@@ -5,6 +5,29 @@ import (
 	"testing"
 )
 
+type testSink[T any] struct {
+	limit        int
+	began, ended bool
+	result       []T
+}
+
+func (s *testSink[T]) begin() {
+	s.began = true
+}
+
+func (s *testSink[T]) done() bool {
+	return s.limit <= 0
+}
+
+func (s *testSink[T]) accept(x T) {
+	s.result = append(s.result, x)
+	s.limit--
+}
+
+func (s *testSink[T]) end() {
+	s.ended = true
+}
+
 func TestChainedSink(t *testing.T) {
 	var s sink[string]
 
